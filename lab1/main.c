899536f6901af632b70f7f57d5324dfdc269d174
Ghanan Gowripalan <ghanan94@gmail.com>
2016-05-14 14:37:55 -0400

    Custom delay with nested for loops works well but 4 second offset after 10 minutes of running

--- lab1/main.c
+++ lab1/main.c
@@ -2,28 +2,16 @@
 #include <lpc17xx.h>
 #include "glcd.h"
 
-int main(void) {
-	SystemInit();
-	GLCD_Init();
-	GLCD_Clear(White);
-	GLCD_DisplayString(0, 0, 1, "Lab1!");
-
-	//doTenMinuteClockWithCustomDelay();
-	//doTenMinuteClockWithTimer();
-
-	return 0;
-}
-
 void customDelay(unsigned int time) {
-	// TODO: Below code is just a sample.
-	// Must figure out the actual values for
-	// a and b.
+	// looping 25000 times seems to create a 
+	// delay of 1 ms, so do that a times to create
+	// a delay of a (in milliseconds).
 
 	unsigned int a;
 	unsigned int b;
 	
 	for(a = time; a != 0; --a) {
-		for (b = 100; b != 0; --b) {
+		for (b = 25000; b != 0; --b) {
 			
 		}
 	}
@@ -37,7 +25,6 @@ void doTenMinuteClockWithCustomDelay() {
 
 	sprintf(str, "%02u:%02u", minutes, seconds);
 
-	GLCD_Clear(White);
 	GLCD_DisplayString(0, 0, 1, str);
 
 	// Disable all interrupts during the delay
@@ -54,7 +41,6 @@ void doTenMinuteClockWithCustomDelay() {
 
 		sprintf(str, "%02u:%02u", minutes, seconds);
 
-		GLCD_Clear(White);
 		GLCD_DisplayString(0, 0, 1, str);
 	}
 
@@ -63,13 +49,49 @@ void doTenMinuteClockWithCustomDelay() {
 }
 
 void doTenMinuteClockWithTimer() {
+	unsigned int minutes = 0;
+	unsigned int seconds = 0;
+
+	char str[6];
+
 	LPC_TIM0->TCR = 0x02; // Reset Timer
 	LPC_TIM0->TCR = 0x01; // Enable Timer
 	LPC_TIM0->MR0 = 2048; // Match value
 	LPC_TIM0->MCR |= 0x03; // On match, generate interrupt and reset
 	NVIC_EnableIRQ(TIMER0_IRQn); // Allow for interrupts from timer0
+
+	sprintf(str, "%02u:%02u", minutes, seconds);
+
+	GLCD_Clear(White);
+	GLCD_DisplayString(0, 0, 1, str);
+
+	while(minutes != 10) {
+		customDelay(1000);
+
+		if (++seconds == 60) {
+			++minutes;
+			seconds = 0;
+		}
+
+		sprintf(str, "%02u:%02u", minutes, seconds);
+
+		GLCD_Clear(White);
+		GLCD_DisplayString(0, 0, 1, str);
+	}
 }
 
 void TIMER0_IRQHander() {
 	
+}
+
+int main(void) {
+	SystemInit();
+	GLCD_Init();
+	GLCD_Clear(White);
+	GLCD_DisplayString(0, 0, 1, "Lab1!");
+
+	doTenMinuteClockWithCustomDelay();
+	//doTenMinuteClockWithTimer();
+
+	return 0;
 }
