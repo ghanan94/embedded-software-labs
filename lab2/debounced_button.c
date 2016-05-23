2004a600f862a0b8d9716844c7ca436487d09f9e
Ghanan Gowripalan <ghanan94@gmail.com>
2016-05-22 23:31:55 -0400

    timer to track time between button press and release for dash/dot determining

--- lab2/debounced_button.c
+++ lab2/debounced_button.c
@@ -36,11 +36,18 @@ struct transition POSSIBLE_BUTTON_TRANSITIONS[] = {
 struct finite_state_machine *debounced_button_fsm;
 
 /*
+ * NAME:          current_time
+ *
+ * DESCRIPTION:   Current time in ms (from start of system).
+ */
+unsigned int current_time;
+
+/*
  * NAME:          last_button_press_time
  *
- * DESCRIPTION:   Time of the last time button press event occured.
+ * DESCRIPTION:   Time of the last button press event.
  */
-int last_button_press_time;
+unsigned int last_button_press_time;
 
 /*
  * NAME:          debounced_button_state_transition
@@ -62,19 +69,59 @@ int last_button_press_time;
 void debounced_button_state_transition(int previous_state, int event, int current_state) {
     switch (event) {
         case BUTTON_PRESS_EVENT:
-            //last_button_press_time = current_time;
+            // Time that the putton was pressed
+            last_button_press_time = current_time;
             break;
         case BUTTON_RELEASE_EVENT:
-            /*if ((current_time - last_button_press_time) >= DASH_DELAY_THRESHOLD_MS) {
+            // If time between last button press and release is greater than
+            // a threshold, it is a DASH; otherwise it is a DOT.
+            if ((current_time - last_button_press_time) >= DASH_DELAY_THRESHOLD_MS) {
                 // DASH occured
                 perform_state_transition(morse_code_fsm, MORSE_CODE_DASH_EVENT);
             } else {
                 // DOT occured
                 perform_state_transition(morse_code_fsm, MORSE_CODE_DOT_EVENT);
-            }*/
+            }
             break;
     }
 }
+
+/*
+ * NAME:          init_timer
+ *
+ * DESCRIPTION:   Initializes and sets up the timer to tick every 1 ms.
+ *
+ * PARAMETERS:
+ *  N/A
+ *
+ * RETURNS:
+ *  N/A
+ */
+ void init_timer(void) {
+     LPC_TIM0->TCR = 0x02; // Reset Timer
+     LPC_TIM0->TCR = 0x01; // Enable Timer
+     LPC_TIM0->MR0 = 25000; // Match value  (M = 100; N = 6; F = 12MHz; CCLKSEL set to divide by 4; PCLK_TIMER0 set to divice by 4
+                            // so 2 * M * F / (N * CCLKSEL_DIV_VALUE * PCLK_DIV_TIMER0 * 1000) = 2 * 100 * 12000000/(6 * 4 * 4 * 1000) = 25000
+     LPC_TIM0->MCR |= 0x03; // On match, generate interrupt and reset
+     NVIC_EnableIRQ(TIMER0_IRQn); // Allow for interrupts from Timer0
+ }
+
+ /*
+  * NAME:          TIMER0_IRQHandler
+  *
+  * DESCRIPTION:   Interrupt handler for TIMER0. Should fire every 1 ms.
+  *
+  * PARAMETERS:
+  *  N/A
+  *
+  * RETURNS:
+  *  N/A
+  */
+ void TIMER0_IRQHandler(void) {
+     LPC_TIM0->IR |= 0x01; // Clear interrupt request
+
+     ++current_time;
+ }
 
 /*
  * NAME:          init_debounced_button_fsm
@@ -105,4 +152,5 @@ void init_debounced_button(void) {
     LPC_GPIO2->FIODIR &= ~(1 << 10); // P2.10 is input
 
     init_debounced_button_fsm();
+    init_timer();
 }
