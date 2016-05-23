6f0fbd0bd6f21cdb812c187bb890b5d929d16b78
Ghanan Gowripalan <ghanan94@gmail.com>
2016-05-22 23:01:00 -0400

    Debounced button fsm init and transition function

--- lab2/debounced_button.c
+++ lab2/debounced_button.c
@@ -1,7 +1,108 @@
-#include <lpc17xx.h>
 #include "debounced_button.h"
+#include <lpc17xx.h>
+#include <stdlib.h>
+#include "morse_code.h"
 
+/*
+ * NAME:          DASH_DELAY_THRESHOLD_MS
+ *
+ * DESCRIPTION:   Minimum time in milliseconds between a button press and
+ *                button release event for it to be considered a dash.
+ */
+int DASH_DELAY_THRESHOLD_MS = 500;
+
+/*
+ * NAME:          NUM_POSSIBLE_BUTTON_TRANSITIONS
+ *
+ * DESCRIPTION:   Total number of button transitions.
+ */
+int NUM_POSSIBLE_BUTTON_TRANSITIONS = 2;
+
+/*
+ * NAME:          POSSIBLE_BUTTON_TRANSITIONS
+ *
+ * DESCRIPTION:   Array of all possible state transitions for button.
+ */
+struct transition POSSIBLE_BUTTON_TRANSITIONS[] = {
+    { BUTTON_RELEASED_STATE, BUTTON_PRESS_EVENT, BUTTON_PRESSED_STATE },
+    { BUTTON_PRESSED_STATE, BUTTON_RELEASE_EVENT, BUTTON_RELEASED_STATE }
+};
+
+/*
+ * NAME:          debounced_button_fsm
+ *
+ * DESCRIPTION:   Pointer to the finite state machine for the debounced button.
+ */
+struct finite_state_machine *debounced_button_fsm;
+
+/*
+ * NAME:          last_button_press_time
+ *
+ * DESCRIPTION:   Time of the last time button press event occured.
+ */
+int last_button_press_time;
+
+/*
+ * NAME:          debounced_button_state_transition
+ *
+ * DESCRIPTION:   Submits an event to the morse code finite state machine
+ *                depending on button actions.
+ *
+ * PARAMETERS:
+ *  int previous_state
+ *    - State before the transition.
+ *  int event
+ *    - Event that triggered a state transition.
+ *  int current_state
+ *    - State transitioned to.
+ *
+ * RETURNS:
+ *  N/A
+ */
+void debounced_button_state_transition(int previous_state, int event, int current_state) {
+    switch (event) {
+        case BUTTON_PRESS_EVENT:
+            //last_button_press_time = current_time;
+            break;
+        case BUTTON_RELEASE_EVENT:
+            /*if ((current_time - last_button_press_time) >= DASH_DELAY_THRESHOLD_MS) {
+                // DASH occured
+                perform_state_transition(morse_code_fsm, MORSE_CODE_DASH_EVENT);
+            } else {
+                // DOT occured
+                perform_state_transition(morse_code_fsm, MORSE_CODE_DOT_EVENT);
+            }*/
+            break;
+    }
+}
+
+/*
+ * NAME:          init_debounced_button_fsm
+ *
+ * DESCRIPTION:   Initializes and sets up the debounced button finite state
+ *                machine.
+ *
+ * PARAMETERS:
+ *  N/A
+ *
+ * RETURNS:
+ *  N/A
+ */
+void init_debounced_button_fsm(void) {
+  debounced_button_fsm = (struct finite_state_machine *) malloc(sizeof(struct finite_state_machine));
+
+  debounced_button_fsm->current_state = BUTTON_RELEASED_STATE;
+  debounced_button_fsm->num_transitions = NUM_POSSIBLE_BUTTON_TRANSITIONS;
+  debounced_button_fsm->transitions = POSSIBLE_BUTTON_TRANSITIONS;
+  debounced_button_fsm->transition_function = &debounced_button_state_transition;
+}
+
+/*
+ * See debounced_button.h for comments.
+ */
 void init_debounced_button(void) {
     LPC_PINCON->PINSEL4 &= ~(3 << 20); // P2.10 is GPIO
     LPC_GPIO2->FIODIR &= ~(1 << 10); // P2.10 is input
+
+    init_debounced_button_fsm();
 }
