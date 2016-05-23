bf04f73eeaf6c780e7914f0f970b4931cb0e927d
Ghanan Gowripalan <ghanan94@gmail.com>
2016-05-23 16:39:53 -0400

    Thermostat init

--- lab2/thermostat/main.c
+++ lab2/thermostat/main.c
@@ -1,11 +1,14 @@
 #include <lpc17xx.h>
 #include "glcd.h"
+#include "thermostat.h"
 
 int main(void) {
     SystemInit();
     GLCD_Init();
     GLCD_Clear(White);
     GLCD_DisplayString(0, 0, 1, "Thermostat");
+
+	init_thermostat();
 
     while(1) {}
 
--- lab2/thermostat/thermostat.c
+++ lab2/thermostat/thermostat.c
@@ -1,1 +1,34 @@
 #include "thermostat.h"
+
+/*
+ * NAME:          NUM_POSSIBLE_THERMOSTAT_TRANSITIONS
+ *
+ * DESCRIPTION:   Total number of thermostat transitions.
+ */
+#define NUM_POSSIBLE_THERMOSTAT_TRANSITIONS 6
+
+/*
+ * NAME:          POSSIBLE_THERMOSTAT_TRANSITIONS
+ *
+ * DESCRIPTION:   Array of all possible state transitions for thermostat.
+ */
+struct transition POSSIBLE_THERMOSTAT_TRANSITIONS[] = {
+    { THERMOSTAT_IDLE_STATE, THERMOSTAT_ACTUAL_TEMPERATURE_SENSED_HOT_EVENT, THERMOSTAT_COOLING_STATE },
+    { THERMOSTAT_IDLE_STATE, THERMOSTAT_ACTUAL_TEMPERATURE_SENSED_COOL_EVENT, THERMOSTAT_HEATING_STATE },
+    { THERMOSTAT_HEATING_STATE, THERMOSTAT_ACTUAL_TEMPERATURE_SENSED_HOT_EVENT, THERMOSTAT_COOLING_STATE },
+    { THERMOSTAT_COOLING_STATE, THERMOSTAT_ACTUAL_TEMPERATURE_SENSED_COOL_EVENT, THERMOSTAT_HEATING_STATE }
+};
+
+/*
+ * See thermostat.h for comments.
+ */
+struct finite_state_machine thermostat_fsm;
+
+/*
+ * See thermostat.h for comments.
+ */
+void init_thermostat(void) {
+    thermostat_fsm.current_state = THERMOSTAT_IDLE_STATE;
+    thermostat_fsm.num_transitions = NUM_POSSIBLE_THERMOSTAT_TRANSITIONS;
+    thermostat_fsm.transitions = POSSIBLE_THERMOSTAT_TRANSITIONS;
+}
