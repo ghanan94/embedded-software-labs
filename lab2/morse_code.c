26319198408d07e6b60eabbbe53a7198b1fe911e
Ghanan Gowripalan <ghanan94@gmail.com>
2016-05-22 21:20:46 -0400

    morse code state transitioning function

--- lab2/morse_code.c
+++ lab2/morse_code.c
@@ -1,6 +1,7 @@
 #include "morse_code.h"
 #include "fsm.h"
 #include <stdlib.h>
+#include "glcd.h"
 
 /*
  * NAME:          NUM_POSSIBLE_MORSE_CODE_TRANSITIONS
@@ -30,10 +31,51 @@ struct transition POSSIBLE_MORSE_CODE_TRANSITIONS[] = {
     { MORSE_CODE_STAGE_6_STATE, MORSE_CODE_DASH_EVENT, MORSE_CODE_STAGE_2_STATE }   // dot dash dash dot dash [dot dash]
 };
 
+unsigned char CORRECT_TEXT[] = "CORRECT\0";
+unsigned char DOT_TEXT[] = "DOT\0";
+unsigned char DASH_TEXT[] = "DASH\0";
+
 /*
  * See morse_code.h for comments.
  */
 struct finite_state_machine *morse_code_fsm;
+
+/*
+ * NAME:          morse_code_state_transition
+ *
+ * DESCRIPTION:   Updates the text on the screen depending on the event and
+ *                current state. If pattern is completed (on stage 7), display
+ *                'CORRECT'. Otherwise, if a dot or a dash event occured,
+ *                display 'DOT' or 'DASH', respectively.
+ *
+ * PARAMETERS:
+ *  int previous_state
+ *    - State before the transition.
+ *  int event
+ *    - Event that triggered a state transition.
+ *  int current_state
+ *    - State transitioned to.
+ *
+ * RETURNS:
+ *  N/A
+ */
+void morse_code_state_transition(int previous_state, int event, int current_state) {
+    if (current_state == MORSE_CODE_STAGE_7_STATE) {
+        GLCD_Clear(White);
+        GLCD_DisplayString(0, 0, 1, CORRECT_TEXT);
+    } else {
+        switch (event) {
+            case MORSE_CODE_DOT_EVENT:
+                GLCD_Clear(White);
+                GLCD_DisplayString(0, 0, 1, DOT_TEXT);
+                break;
+            case MORSE_CODE_DASH_EVENT:
+                GLCD_Clear(White);
+                GLCD_DisplayString(0, 0, 1, DASH_TEXT);
+                break;
+        }
+    }
+}
 
 /*
  * See morse_code.h for comments.
@@ -45,4 +87,5 @@ void init_morse_code_fsm(void) {
     morse_code_fsm->current_state = MORSE_CODE_STAGE_0_STATE;
     morse_code_fsm->num_transitions = NUM_POSSIBLE_MORSE_CODE_TRANSITIONS;
     morse_code_fsm->transitions = POSSIBLE_MORSE_CODE_TRANSITIONS;
+    morse_code_fsm->transition_function = &morse_code_state_transition;
 }
