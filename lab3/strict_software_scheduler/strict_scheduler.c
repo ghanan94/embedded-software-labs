84fcfab5c05c1c1482f0b58743ca06c430102ecf
Ghanan Gowripalan <ghanan94@gmail.com>
2016-06-22 13:55:00 -0400

    Strict scheduling implemented with 10s min time between int0 interrupts

--- lab3/strict_software_scheduler/strict_scheduler.c
+++ lab3/strict_software_scheduler/strict_scheduler.c
@@ -6,7 +6,7 @@
  *
  * DESCRIPTION:   Minimum time in milliseconds between each allowed interrupt.
  */
-#define MIN_TIME_BETWEEN_INTERRUPTS_MS 10
+#define MIN_TIME_BETWEEN_INTERRUPTS_MS 10000
 
 /*
  * NAME:          TIMER0_IRQHandler
@@ -25,13 +25,38 @@
  */
 void TIMER0_IRQHandler(void) {
     LPC_TIM0->IR |= 0x01; // Clear interrupt request
+
+    // Re-enable interrupts for INT0 after the minimum time has passed.
+    // Clear any active interrupt requests incase there are any.
+    NVIC_ClearPendingIRQ(EINT3_IRQn);
+    NVIC_EnableIRQ(EINT3_IRQn);
+}
+
+/*
+ * NAME:          EINT3_IRQHandler
+ *
+ * DESCRIPTION:   Interrupt handler for INT0.
+ *
+ * PARAMETERS:
+ *  N/A
+ *
+ * RETURNS:
+ *  N/A
+ */
+void EINT3_IRQHandler(void) {
+    LPC_GPIOINT->IO2IntClr |= 1 << 10; // Clear interrupt on P2.10
+
+    // Disable Interrupts for some time.
+    NVIC_DisableIRQ(EINT3_IRQn);
+    LPC_TIM0->TCR = 0x01; // Enable timer that will enable interrupts again
+                          // after some time.
 }
 
 /*
  * NAME:          init_timer
  *
- * DESCRIPTION:   Initializes and sets up the timer to tick every
- *                <TIME_BETWEEN_BUTTON_READS_MS> ms.
+ * DESCRIPTION:   Initializes and sets up the timer to tick after
+ *                <MIN_TIME_BETWEEN_INTERRUPTS_MS> ms.
  *
  * PARAMETERS:
  *  N/A
@@ -48,7 +73,7 @@ void init_timer(void) {
     // so 2 * M * F / (N * CCLKSEL_DIV_VALUE * PCLK_DIV_TIMER0 * 1000) = 2 * 100 * 12000000/(6 * 4 * 4 * 1000) = 25000
     // Multiplying above value by MIN_TIME_BETWEEN_INTERRUPTS_MS allows us to only
     // have to call the ISR when the minimum time between interrupts has passed
-		// and we can re-enable interrupts.
+    // and we can re-enable interrupts.
     LPC_TIM0->MR0 = 25000 * MIN_TIME_BETWEEN_INTERRUPTS_MS;
     LPC_TIM0->MCR |= 0x07; // On match, generate interrupt, reset and stop counting
     NVIC_EnableIRQ(TIMER0_IRQn); // Allow for interrupts from Timer0
@@ -58,5 +83,10 @@ void init_timer(void) {
  * See strict_scheduler.h for comments.
  */
 void init_strict_scheduled_button(void) {
+    LPC_PINCON->PINSEL4 &= ~(3 << 20); // P2.10 (INT0) is GPIO
+    LPC_GPIO2->FIODIR &= ~(1 << 10);   // P2.10 (INT0) is input
+    LPC_GPIOINT->IO2IntEnR |= 1 << 10; // Interrupt on rising edge of P2.10
+    NVIC_EnableIRQ(EINT3_IRQn);
+
     init_timer();
 }
